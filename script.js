161fb3e8ae3c04995b0f7d935ca9f8b78763621d
Ifeanyi Okeke <okekeifeanyi288@gamil.com>
2025-12-06 15:42:58 +0100

    Add Local Storage support for tasks

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function loadTasks() {
     const storedTasks = JSON.parse(localStorage.getItem("tasks") || "[]");
-    storedTasks.forEach((taskText) => addTask(taskText, false)); // false prevents saving again
+    storedTasks.forEach((taskText) => addTask(taskText, false));
   }
 
   function addTask(taskText = null, save = true) {
